ebe6f9d1aca6bd2b75c42de8cfb414aba5b2a5ed
Taeung <taeungjang@naver.com>
2024-07-14 21:20:53 +0900

    Updated ~#3.6

--- src/server.js
+++ src/server.js
@@ -4,16 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
-const hanldeHome = (req, res) => {
-    return res.send("<h1>This is Home.</h1>");
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
 }
 
-const handleLogin = (req, res) => {
-    return res.send("This is Login Home.");
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("You may continue.");
+    next();
 }
 
-app.get("/", hanldeHome);
-app.get("/login", handleLogin);
+const handleHome = (req, res) => {
+    return res.send("HI !");
+}
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost${PORT} 🚀`)
 
