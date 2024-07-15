d035133415c8f695152601fb272c51695374549d
Taeung-Laptop <taeungjang@naver.com>
2024-07-15 16:56:41 +0900

    Updated ~#3

--- src/server.js
+++ src/server.js
@@ -1,35 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("You may continue.");
-    next();
-}
+const loggerMiddleware = morgan("dev");
 
 const handleHome = (req, res) => {
     return res.send("HI !");
 }
 
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge.");
-}
-
-app.use(logger);
-app.use(privateMiddleware);
+app.use(loggerMiddleware);
 app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost${PORT} 🚀`)
 
