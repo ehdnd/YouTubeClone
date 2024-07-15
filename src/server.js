79a1a9b83296f93d6fa8bce0e629f93674ba7a27
Taeung-Laptop <taeungjang@naver.com>
2024-07-15 23:09:58 +0900

    Updated ~#4.3

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const loggerMiddleware = morgan("dev");
 
-const handleHome = (req, res) => {
-    return res.send("HI !");
-}
-
 app.use(loggerMiddleware);
-app.get("/", handleHome);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost${PORT} 🚀`)
 
