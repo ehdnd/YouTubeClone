02e497e07db8e4a0c872394a75e6d12452623926
Taeung <taeungjang@naver.com>
2024-07-23 18:40:43 +0900

    Updated

--- src/server.js
+++ src/server.js
@@ -15,6 +15,6 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost${PORT} 🚀`)
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`)
 
 app.listen(PORT, handleListening);
