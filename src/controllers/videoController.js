a9277a0f5b338a5e7ce90a937e4d9474b828bf18
Taeung <taeungjang@naver.com>
2024-08-01 20:26:18 +0900

    Updated ~#4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,10 @@
 export const trending = (req, res) => res.send("Home Page Trending Videos");
-export const watch = (req, res) => res.send("Watch");
+export const search = (req, res) => res.send("Search");
+export const see = (req, res) => {
+    return res.send(`Watch Video #${req.params.id}`);
+};
 export const edit = (req, res) => res.send("Edit");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+    return res.send("Delete Video");
+};
