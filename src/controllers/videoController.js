32af1704be6070e7c915f708cdd32c6bdc2cabe0
Taeung <taeungjang@naver.com>
2024-08-06 19:10:02 +0900

    Updated ~#5 half

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,7 @@
-export const trending = (req, res) => res.send("Home Page Trending Videos");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
     return res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -9,8 +9,9 @@ const PORT = 4000;
 const app = express();
 const loggerMiddleware = morgan("dev");
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(loggerMiddleware);
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
