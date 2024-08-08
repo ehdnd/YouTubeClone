36995cfa0bfdfb9dbe5717f7e5a1e8c8a1f22b0c
ehdnd_laptop <taeungjang@naver.com>
2024-08-08 17:26:38 +0900

    Prettier reinstalled

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`)
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
