616c3df4e529a755ead88275727ab0424ffb955f
ehdnd <taeung@TAEUNG>
2024-08-08 16:31:20 +0900

    ~#5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,14 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+const fakeUser = {
+  username: "Taeung",
+  loggedIn: true,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const search = (req, res) => res.send("Search");
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
+  return res.send("Delete Video");
 };
