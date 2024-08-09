1a7d12e92474e0ac4e0a3b551681fd9560c6b55a
ehdnd <taeungjang@naver.com>
2024-08-09 23:17:21 +0900

    #6.12 Our First Query part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,12 @@
 import Video from "../models/Video";
 
 export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+  Video.find({}, (error, videos) => {
+    return res.render("home", { pageTitle: "Home", videos: [] });
+  });
 };
+
+
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching` });
