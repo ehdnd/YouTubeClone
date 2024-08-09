b7c0d4ca175417a09fab00febd21e3e385e6dca2
ehdnd_laptop <taeungjang@naver.com>
2024-08-09 16:47:50 +0900

    Minor changes

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -49,9 +49,9 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 }
 export const postUpload = (req, res) => {
-  // here we will add a video to the videos array
+  const { title } = req.body;
   const newVideo = {
-    title: req.body.title,
+    title,
     rating: 0,
     comments: 0,
     createdAt: "just now",
