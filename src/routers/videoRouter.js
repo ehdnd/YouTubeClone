5c944878edc3f6631f8f58c6e9ffe3996b7e7a41
ehdnd <taeung@TAEUNG>
2024-08-09 14:48:06 +0900

    Cleaning Routers

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,10 @@
 import express from "express";
-import { upload, see, getEdit, postEdit, deleteVideo } from "../controllers/videoController";
+import { upload, watch, getEdit, postEdit, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", getEdit);
 videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
