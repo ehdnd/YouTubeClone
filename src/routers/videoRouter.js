af2a1916244ba1f49b452ba086df87e16c2f8838
ehdnd <taeung@TAEUNG>
2024-08-09 14:45:21 +0900

    #6.2 Edit Video part One

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { upload, see, edit, deleteVideo } from "../controllers/videoController";
+import { upload, see, getEdit, postEdit, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
