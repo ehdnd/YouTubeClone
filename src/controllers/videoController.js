fe5e2c4270f93e0b44fedfe6898b1f897bff720d
ehdnd <taeungjang@naver.com>
2024-08-10 16:50:40 +0900

    #6.15 Creating a Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,6 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching` });
@@ -23,6 +22,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 }
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map(word => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
   return res.redirect("/");
 }
