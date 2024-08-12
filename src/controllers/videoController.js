edc716f1f7f6fe826672141dbce5f78a83daba5a
ehdnd <taeungjang@naver.com>
2024-08-12 15:23:34 +0900

    #6.23 Middlewares

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,6 +4,7 @@ export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -12,6 +13,7 @@ export const watch = async (req, res) => {
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
+
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -20,29 +22,33 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
+
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found. "}) 
   }
-  video.title = title;
-  video.description = description;
-  video.hashtags = hashtags.split(",").map(word => word.startsWith("#") ? word : `#${word}`);
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags.split(",").map(word => word.startsWith("#") ? word : `#${word}`)
+  });
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map(word => `#${word}`),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
