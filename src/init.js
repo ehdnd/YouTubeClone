59f732f76674b071e4f5e6c50aa88827f548fa78
ehdnd <taeungjang@naver.com>
2024-08-29 19:49:11 +0900

    committer test

--- src/init.js
+++ src/init.js
@@ -8,4 +8,4 @@ const PORT = 4000;
 const handleListening = () =>
     console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
   
-  app.listen(PORT, handleListening);
+app.listen(PORT, handleListening);
