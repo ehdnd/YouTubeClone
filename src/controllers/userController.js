ac0677f39cf2ed6143f7c85eda75c3694356b39c
ehdnd_desktop <taeungjang@naver.com>
2025-02-23 15:49:11 +0900

    #7.5 Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,17 +21,41 @@ export const postJoin = async (req, res) => {
     });
   }
 
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+  } catch (error) {
+    return res.status(400).render("upload", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
 
   return res.redirect("/login");
 };
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  // check if account exists
+  // check if password correct
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res
+      .status(400)
+      .render("login", {
+        pageTitle: "Login",
+        errorMessage: "An account with this username does not exists.",
+      });
+  }
+
+  res.end();
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log Out");
