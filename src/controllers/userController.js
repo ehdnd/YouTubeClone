4e019e58dcb1336079afaea79010f06643a53d0a
ehdnd_desktop <taeungjang@naver.com>
2025-02-23 16:09:56 +0900

    #7.6 Login part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,11 @@
 import User from "../models/User";
+import bcrypt from "bcryptjs";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password1, password2, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
-  if (password1 != password2) {
+  if (password != password2) {
     return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match",
@@ -30,7 +31,7 @@ export const postJoin = async (req, res) => {
       location,
     });
   } catch (error) {
-    return res.status(400).render("upload", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: error._message,
     });
@@ -41,20 +42,25 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 export const postLogin = async (req, res) => {
-  // check if account exists
-  // check if password correct
   const { username, password } = req.body;
-  const exists = await User.exists({ username });
-  if (!exists) {
-    return res
-      .status(400)
-      .render("login", {
-        pageTitle: "Login",
-        errorMessage: "An account with this username does not exists.",
-      });
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
   }
 
-  res.end();
+  console.log("LOG USER IN. COMMING SOON!");
+  res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
