3d150aeb3f9ec3160cacb936202962206fadf4b4
ehdnd_desktop <taeungjang@naver.com>
2025-02-24 16:42:30 +0900

    #7.15 Environment Variables

--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
-import "./db"
+import "dotenv/config";
+import "./db";
 import "./models/Video";
 import "./models/User";
 import app from "./server";
@@ -6,6 +7,6 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () =>
-    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-  
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
 app.listen(PORT, handleListening);
