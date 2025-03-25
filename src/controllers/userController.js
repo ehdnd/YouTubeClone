af4b2ca25d38765cb8b016f10bf570f269c151ff
ehdnd_desktop <taeungjang@naver.com>
2025-03-25 16:59:19 +0900

    #8.12 Video Owner part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcryptjs";
+import Video from "../models/Video";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -210,8 +211,10 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
+  const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
     pageTitle: `${user.name} Profile`,
     user,
+    videos,
   });
 };
