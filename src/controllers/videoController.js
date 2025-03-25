3b595f4bbc294b210f1f7c765b818b4cbfdebe7d
ehdnd_desktop <taeungjang@naver.com>
2025-03-25 20:07:05 +0900

    #8.14 Bugfix - fixed postEdit w/ GPT

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,7 +36,7 @@ export const postEdit = async (req, res) => {
     user: { _id },
   } = req.session;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({ _id: id });
+  const video = await Video.findById(id);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found. " });
   }
